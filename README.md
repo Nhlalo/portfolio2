8debfcaa3921899192b940ba6534f09eecb34603
Nhlalo <95585504+Nhlalo@users.noreply.github.com>
2025-02-18 13:27:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProjectRepository
+
+This is a template to increase productivity by bypassing the setup phase. This template can be updated to suit a specific project.
---
+++ webpack.common.js
@@ -0,0 +1,54 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    watchFiles: ["./src/html/index.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            targets: "defaults",
+            presets: [
+              ['@babel/preset-env']
+            ]
+          }
+        }
+      }
+    ],
+  },
+  stats: {
+    loggingDebug: ["babel-loader"]
+  },
+};
